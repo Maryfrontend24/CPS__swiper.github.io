165c64fad67bdc036d90b205df2bc40eae6684fb
Mary <137189389+Maryfrontend24@users.noreply.github.com>
2024-07-02 19:26:59 +0300

    swiper

---
+++ main.js
@@ -0,0 +1,56 @@
+var arrowLinkClass = document.querySelector('.arrow-link');
+
+arrowLinkClass.addEventListener('click', (event) => {
+    event.preventDefault();
+    let itemsListClass = document.querySelector('.items__list');
+    if (itemsListClass.classList.contains('unset-block')&& arrowLinkClass.classList.contains('arrow-link--open')) {
+        itemsListClass.classList.remove('unset-block');
+        arrowLinkClass.classList.remove('arrow-link--open');
+
+
+        return;
+    }
+    itemsListClass.classList.add('unset-block');
+    arrowLinkClass.classList.add('arrow-link--open');
+})
+//
+
+
+
+
+
+
+
+
+let mql = window.matchMedia('(max-width: 767px)');
+
+window.addEventListener('resize', () => {
+    if (mql.matches) {
+        let newSwiper = new Swiper('.swiper', {
+            slidesPerView: 1.5,
+            centeredSlides: false,
+            spaceBetween: 16,
+            direction: 'horizontal',
+            loop: true,
+            speed: 1000,
+            freeSwipe: true,
+            init: true,
+
+            pagination: {
+                    el: '.swiper-pagination',
+                    clickable: true,
+                },
+
+            controller: {
+                invert: true,
+            },
+            autoplay: {
+                delay: 2000,
+            },
+        });
+    } else {
+        swiper.destroy();
+    }
+})
+
+
